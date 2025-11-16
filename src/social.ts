0d7b88a9122b8b38abac9a16bf6bd9a1f8b6c47f
Lyricus <2490665576@qq.com>
2025-11-16 15:30:53 +0800

    update

---
+++ src/social.ts
@@ -0,0 +1,14 @@
+export const social = {
+    "github": {
+        "url": "https://github.com/Lyricus233",
+        "icon": ""
+    },
+    "twitter": {
+        "url": "",
+        "icon": ""
+    },
+    "telegram": {
+        "url": "",
+        "icon": ""
+    }
+};
